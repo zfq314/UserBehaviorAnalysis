e2ad46b5c2e4fda258de08f61afb1711d5989d41
赵富强 <zfq0314@163.com>
2019-12-10 19:20:29 +0800

    用户行为样例类注释

--- HotItemsAnalysis/src/main/scala/com/atguigu/hotitems_analysis/pojo/UserBehavior.scala
+++ HotItemsAnalysis/src/main/scala/com/atguigu/hotitems_analysis/pojo/UserBehavior.scala
@@ -1,4 +1,5 @@
 package com.atguigu.hotitems_analysis.pojo
+//用户用为样例类
 //增加注释
 case class UserBehavior(userId: Long, itemId: Long, categoryId: Int,
                         behavior: String, timestamp: Long) {
