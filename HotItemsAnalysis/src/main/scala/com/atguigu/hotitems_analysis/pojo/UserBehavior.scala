e0e153b1a59a2e82f711f30c1dd6c0bb970fae83
zfq314 <33599707+zfq314@users.noreply.github.com>
2019-12-10 19:14:23 +0800

    Update UserBehavior.scala

--- HotItemsAnalysis/src/main/scala/com/atguigu/hotitems_analysis/pojo/UserBehavior.scala
+++ HotItemsAnalysis/src/main/scala/com/atguigu/hotitems_analysis/pojo/UserBehavior.scala
@@ -1,5 +1,5 @@
 package com.atguigu.hotitems_analysis.pojo
-
+//用户行为样例类增加注释
 case class UserBehavior(userId: Long, itemId: Long, categoryId: Int,
                         behavior: String, timestamp: Long) {
 
